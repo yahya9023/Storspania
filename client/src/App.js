3e5112cb5c3dd79aa543ddaf357b2587c27a1adc
Yahia <yahyaelhabchi476@gmail.com>
2025-05-23 14:10:16 +0200

    add route register on page app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProductPage from "./pages/AddProductPage"; 
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<h1>Welcome to Spain Store</h1>} />
         <Route path="/add-product" element={<AddProductPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
